5fc6b0311103709d29bb569e08363a1b1501b41f
xxxinymph20171105406 <43673484+xxxinymph20171105406@users.noreply.github.com>
2019-06-18 15:56:53 +0800

    加了Metadata-extractor.jar。
    Metadata-extractor.jar的包可以识别图片的exif信息。

--- PHOTO/src/cn/edu/imnu/photo/ReadPhoto.java
+++ PHOTO/src/cn/edu/imnu/photo/ReadPhoto.java
@@ -1,16 +1,29 @@
 package cn.edu.imnu.photo;
 
 import java.io.File;
+import java.io.IOException;
+
+
+import com.drew.imaging.ImageProcessingException;
+import com.drew.imaging.jpeg.JpegMetadataReader;
+import com.drew.imaging.jpeg.JpegProcessingException;
+import com.drew.metadata.Metadata;
 
 public class ReadPhoto {
-	public static void main(String[] args) {
-		File jpegFile = new File("C:\\Users\\hp\\Pictures\\壁纸.JPG");
-		
-		Metadata metadata = JpegMetadataReader.readMetadata(jpegFile);
-		
-    }  
+	public static void main(String[] args) throws JpegProcessingException, IOException{
+		File file = new File("‪C:\\Users\\hp\\Desktop\\java\\campus.jpg");
+		System.out.println("File Name: " + file.getName());
+		Metadata metadata = JpegMetadataReader.readMetadata(file);
+			
+
+
+	
+	}
+
+	
+
+}  
 	
 	
 	
 
-}
