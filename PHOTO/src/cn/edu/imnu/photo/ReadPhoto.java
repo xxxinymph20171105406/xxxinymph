0077183467e35578cb0a3304eb8833da9f008552
xxxinymph20171105406 <43673484+xxxinymph20171105406@users.noreply.github.com>
2019-06-22 23:02:19 +0800

    可以出来exif信息。
    可以出来exif的信息，但是宽度和高度会重复出现几次。

--- PHOTO/src/cn/edu/imnu/photo/ReadPhoto.java
+++ PHOTO/src/cn/edu/imnu/photo/ReadPhoto.java
@@ -4,20 +4,43 @@ import java.io.File;
 import java.io.IOException;
 
 
+import com.drew.imaging.ImageMetadataReader;
 import com.drew.imaging.ImageProcessingException;
-import com.drew.imaging.jpeg.JpegMetadataReader;
-import com.drew.imaging.jpeg.JpegProcessingException;
+import com.drew.metadata.Directory;
 import com.drew.metadata.Metadata;
+import com.drew.metadata.Tag;
+
 
 public class ReadPhoto {
-	public static void main(String[] args) throws JpegProcessingException, IOException{
-		File file = new File("‪C:\\Users\\hp\\Desktop\\java\\campus.jpg");
+	public static void main(String[] args) throws Exception, Exception{
+		File file = new File("C:\\Users\\hp\\Desktop\\java\\meal.jpg");
 		System.out.println("File Name: " + file.getName());
-		Metadata metadata = JpegMetadataReader.readMetadata(file);
-			
+		printImageTags(file);
 
+	}
 
-	
+	private static void printImageTags(File file) throws ImageProcessingException ,Exception{
+		Metadata metadata = ImageMetadataReader.readMetadata(file);
+		for(Directory directory : metadata.getDirectories()){
+			for(Tag tag : directory.getTags()) {
+				String tagName = tag.getTagName();
+				String info = tag.getDescription();
+				if(tagName.equals("Image Height")) {
+					System.out.println("图片高度 ： " + info);
+				}else if(tagName.equals("Image Width")) {
+					System.out.println("图片宽度 ： " + info);
+				}else if(tagName.equals("Date/Time Original")) {
+					System.out.println("拍摄时间 ： " + info);
+				}else if(tagName.equals("GPS Latitude")) {
+					System.out.println("维度：" + info);
+				}else if(tagName.equals("GPS Longitude")) {
+					System.err.println("经度： " + info);
+				}
+				
+				
+			}
+		}
+		
 	}
 
 	
